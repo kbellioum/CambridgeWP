15b8ad85035085985b766043be13e3bd05392098
kbellioum <kbellioum@gmail.com>
2016-09-19 20:52:19 +0100

    before git pull

--- routes/index.js
+++ routes/index.js
@@ -816,9 +816,9 @@ router.delete('/listprog/:prog_id', isAuthenticated, function(req, res){
 
 	router.get('/test', isAuthenticated, function(req, res){
 
-    var permissions = JSON.parse(req.user.permissions);
 
-    res.render('test', {user: req.user, tt: "Prog 1", permissions: permissions });
+
+    res.render('test', {user: req.user, tt: "Prog 1"});
 
 
   });
